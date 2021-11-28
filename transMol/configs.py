c7383e867a7fb0ad7c50d609f4b4f3bc8d9f3471
tanjundong <jundong@ubuntu20.4>
2021-11-28 12:00:23 +0800

    iaa

--- transMol/configs.py
+++ transMol/configs.py
@@ -1,17 +1,17 @@
 from tokenizer import SmilesTokenizer
 tokenizer = SmilesTokenizer.load('./a.vocab')
-gpus = [0,1,]
-#gpus = [0,1,2,3,]
+#gpus = [0,1,]
+gpus = [0,1,2,3,4,5]
 #gpus = [4,5,6,7,]
 configs = {
     'hidden_dim': 768,
     'ff_dim': 1024,
     'max_len': 80,
     'vocab_size': 100,
-    'n_heads': 8,
-    'n_encode_layers': 4,
-    'n_decode_layers': 4,
-    'max_kl_weights' : 2.0,
+    'n_heads': 4,
+    'n_encode_layers': 10,
+    'n_decode_layers': 10,
+    'max_kl_weights' : 5.0,
     'max_epoch': 300,
     'decoder': 'GPT',
     'decode_from_latent': True,
